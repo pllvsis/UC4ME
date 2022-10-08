dc819bda4a57efa7abfda06dd586d7556d17e307
pllvsis <pllvsisodiya@gmail.com>
2022-10-08 16:35:54 -0400

    routes page added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
+import RoutesListPage from '../views/RoutesListPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    component: HomePage,
+    component: RoutesListPage,
   }
 ]
 
@@ -15,3 +16,4 @@ const router = createRouter({
 })
 
 export default router
+
---
+++ src/views/RoutesListPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <ion-page>
+    <ion-content class="ion-padding" :fullscreen="true">
+      <ion-title size="large">Routes</ion-title>
+
+      <ion-grid class="ion-align-item-center">
+        <ion-list id="route-list">
+          <li v-for="route in routes" :key="route.routeName" class="route-item">
+            <ion-row class="ion-justify-content-evenly ion-padding text-wrap">
+              <ion-button class="route-button">
+                <ion-label text-wrap>
+                  {{ route.routeName }}
+                </ion-label>
+              </ion-button>
+            </ion-row>
+          </li>
+        </ion-list>
+      </ion-grid> </ion-content
+    >>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { IonPage, IonContent, IonButton } from "@ionic/vue";
+import { identifier } from "@babel/types";
+
+export default defineComponent({
+  name: "RoutesListPage",
+  components: { IonContent, IonPage, IonButton },
+  data() {
+    return {
+      routes: [
+        { routeName: "Home" },
+        { routeName: "Work" },
+        { routeName: "Campus"},
+        { routeName: "Buckeye Corner"},
+        { routeName: "Union Square"}
+    ],
+    };
+  },
+  methods: {},
+});
+</script>
+
+<style>
+#route-list li {
+  list-style: none;
+}
+
+ion-button {
+  font-size: 1.7em;
+  height: 120px;
+  width: 90%;
+}
+</style>
