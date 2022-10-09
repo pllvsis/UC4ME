1d55715c46ebf44bc5d8e768fcf7090cd36ac338
Parth Kharwar <kharwarparth@gmail.com>
2022-10-09 01:12:55 -0400

    refactor: updates styling for adventurer page

--- src/views/AdventurePage.vue
+++ src/views/AdventurePage.vue
@@ -1,53 +1,53 @@
 <template>
-    <ion-page>
-        <ion-content>
-            <ion-grid>
-                <ion-row class="ion-justify-content-evenly ion-padding">
-                    <ion-card class="nav-card">
-                        <ion-card-content>
-                            {{ variableSomething }}
-                        </ion-card-content>
-                    </ion-card>
-                </ion-row>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Navigating</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-grid>
+        <ion-row class="ion-padding">
+          <ion-item class="navigation-instruction-card">
+            <ion-label class="ion-text-wrap">Instruction</ion-label>
+          </ion-item>
+          <ion-item class="navigation-instruction-card">
+            <ion-label class="ion-text-wrap">Instruction</ion-label>
+          </ion-item>
+        </ion-row>
 
-                <ion-row class="ion-justify-content-evenly ion-padding">
-                    <ion-card class="nav-card">
-                        <ion-card-content>
-                            Right Turn
-                        </ion-card-content>
-                    </ion-card>
-                </ion-row>
-
-                <ion-row class="ion-justify-content-evenly ion-padding">
-                    <ion-button aria-label="Start navigation" id="start-button" href="/adventure">Exit Navigation
-                    </ion-button>
-                </ion-row>
-
-            </ion-grid>
-        </ion-content>
-    </ion-page>
+        <ion-row class="ion-justify-content-evenly ion-padding">
+          <ion-button
+            aria-label="Start navigation"
+            id="start-button"
+            href="/adventure"
+            >Exit Navigation
+          </ion-button>
+        </ion-row>
+      </ion-grid>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
-
 import { defineComponent } from "vue";
-import { IonPage, IonContent, IonCard, IonCardContent } from "@ionic/vue";
+import { IonPage, IonContent } from "@ionic/vue";
 
 export default defineComponent({
-    name: "AdventurePage",
-    components: { IonCard, IonCardContent, IonContent, IonPage },
-    data() {
-        return {
-            variableSomething: ""
-        }
-    },
+  name: "AdventurePage",
+  components: { IonContent, IonPage },
+  data() {
+    return {
+      variableSomething: "",
+    };
+  },
 });
 </script>
 
 <style>
-ion-grid {
-    height: 100%;
-    width: 90%;
-    display: grid;
+.navigation-instruction-card {
+  width: 100%;
+  font-size: 1.75em;
+  text-align: center;
 }
 </style>
